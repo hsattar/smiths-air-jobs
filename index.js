2291c4901b44a65055fbb8fb60e35558c3eda282
Hasan Sattar <hasanraza1798@gmail.com>
2023-01-16 16:44:17 +0000

    completed web scraping

---
+++ index.js
@@ -0,0 +1,26 @@
+import playwright from 'playwright'
+
+(async () => {
+    const browser = await playwright.chromium.launch({ headless: true })
+    const page = await browser.newPage()
+    await page.goto('https://whsmithcareers.co.uk')
+    await page.click('.job-link')
+    await page.waitForSelector('[name=keyword]', { visible: true })
+    await page.fill('[name=keyword]', 'Manchester Airport')
+    await page.click('[type=submit]')
+    await page.waitForSelector('.tr.jobs.hidden-xs', { visible: true })
+    await page.waitForSelector("a[href^='https://krb-sjobs.brassring.com']", { visible: true })
+
+    const data = await page.evaluate(() => {
+        const jobs = document.querySelectorAll('.tr.jobs.hidden-xs')
+        const jobsText = Array.from(jobs).map(job => job.innerText)
+        const jobsHrefs = document.querySelectorAll("a[href^='https://krb-sjobs.brassring.com']")
+        const jobLinks = Array.from(jobsHrefs).map(job => job.href)
+        const uniqueJobLinks = [...new Set(jobLinks)]
+        return { uniqueJobLinks, jobsText }
+    })
+
+    await browser.close()
+    console.log(data)
+    return data
+})()  
